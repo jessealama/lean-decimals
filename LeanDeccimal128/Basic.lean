2f8ba86b90df6cbf48280fdd82a3aba3ce54822a
Jesse Alama <jesse@serverracket.com>
2024-06-26 12:10:58 +0200

    Getting started

--- LeanDeccimal128/Basic.lean
+++ LeanDeccimal128/Basic.lean
@@ -1,1 +1,28 @@
-def hello := "world"
+import Std.Data.Rat
+
+structure BigInt where
+  isNegative: Bool
+  significand: Nat
+  exponent: Int
+deriving Repr
+
+def ratOne := Rat.mk' 1 1
+def ratTen := Rat.mk' 10 1
+
+def ratExp (x : Rat) (n : Nat) : Rat :=
+  if n == 0 then ratOne
+  else Rat.mul x (ratExp x (n - 1))
+
+def tenToPower (n : Int) : Rat :=
+    if n < 0 then Rat.div ratOne
+                          (tenToPower (-n))
+    else if n == 0 then (Rat.mk' 1 1)
+    else Rat.mul ratTen
+                 (tenToPower (n - 1))
+
+def MathematicalValue (x: BigInt) : Rat =
+  if x.isNegative then
+    Rat.mul ratOne MathematicalValue { x with isNegative := False }
+  else
+    Rat.mul (Rat.mk' x.significand 1)
+            (ratExp ratTen x.exponent)
--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,8 @@
 import Lake
 open Lake DSL
 
+ require std from git "https://github.com/leanprover/std4" @ "main"
+
 package «lean-deccimal128» where
   -- add package configuration options here
 
