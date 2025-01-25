9f94f18e5ec37077ec3a56fa327cab022d5fd0ad
Jesse Alama <hello@jessealama.net>
2025-01-25 07:11:42 +0100

    Define multiplication and division

--- Decimal128/Basic.lean
+++ Decimal128/Basic.lean
@@ -40,6 +40,13 @@ def isZero (x : Decimal128Value) : Bool :=
   | Decimal128Value.NegZero => true
   | _ => false
 
+def isNegative (x : Decimal128Value) : Bool :=
+  match x with
+  | Decimal128Value.NegInfinity => true
+  | Decimal128Value.NegZero => true
+  | Decimal128Value.Rational ⟨q, _⟩ => q < 0
+  | _ => false
+
 def isFinite (x : Decimal128Value) : Bool :=
   match x with
   | Decimal128Value.NaN => false
@@ -161,3 +168,40 @@ def sub (x : Decimal128Value) (y : Decimal128Value) : Decimal128Value :=
   | Decimal128Value.NegZero, _ => y
   | _, Decimal128Value.NegZero => x
   | Decimal128Value.Rational x, Decimal128Value.Rational y => RoundToDecimal128Domain (x.val - y.val) RoundingMode.halfEven
+
+def multiply (x : Decimal128Value) (y : Decimal128Value) : Decimal128Value :=
+  match x, y with
+  | Decimal128Value.NaN, _ => Decimal128Value.NaN
+  | _, Decimal128Value.NaN => Decimal128Value.NaN
+  | Decimal128Value.NegInfinity, _ => if isZero y then Decimal128Value.NaN else Decimal128Value.NegInfinity
+  | _, Decimal128Value.NegInfinity => if isZero x then Decimal128Value.NaN else Decimal128Value.NegInfinity
+  | Decimal128Value.PosInfinity, _ => if isZero y then Decimal128Value.NaN else Decimal128Value.PosInfinity
+  | _, Decimal128Value.PosInfinity => if isZero x then Decimal128Value.NaN else Decimal128Value.PosInfinity
+  | Decimal128Value.PosZero, _ => Decimal128Value.PosZero
+  | _, Decimal128Value.PosZero => Decimal128Value.PosZero
+  | Decimal128Value.NegZero, Decimal128Value.NegZero => Decimal128Value.PosZero
+  | Decimal128Value.NegZero, Decimal128Value.Rational _ => Decimal128Value.PosZero
+  | Decimal128Value.Rational _, Decimal128Value.NegZero => Decimal128Value.PosZero
+  | Decimal128Value.Rational x, Decimal128Value.Rational y => RoundToDecimal128Domain (x.val * y.val) RoundingMode.halfEven
+
+def divide (x : Decimal128Value) (y : Decimal128Value) : Decimal128Value :=
+  match x, y with
+  | Decimal128Value.NaN, _ => Decimal128Value.NaN
+  | _, Decimal128Value.NaN => Decimal128Value.NaN
+  | Decimal128Value.PosInfinity, Decimal128Value.PosInfinity => Decimal128Value.NaN
+  | Decimal128Value.PosInfinity, Decimal128Value.NegInfinity => Decimal128Value.NaN
+  | Decimal128Value.PosInfinity, _ => if isNegative y then Decimal128Value.NegInfinity else Decimal128Value.PosInfinity
+  | Decimal128Value.NegInfinity, Decimal128Value.NegInfinity => Decimal128Value.NaN
+  | Decimal128Value.NegInfinity, Decimal128Value.PosInfinity => Decimal128Value.NaN
+  | Decimal128Value.NegInfinity, _ => if isNegative y then Decimal128Value.PosInfinity else Decimal128Value.NegInfinity
+  | _, Decimal128Value.PosInfinity => if isNegative x then Decimal128Value.NegZero else Decimal128Value.PosZero
+  | _, Decimal128Value.NegInfinity => if isNegative x then Decimal128Value.PosZero else Decimal128Value.NegZero
+  | Decimal128Value.PosZero, Decimal128Value.PosZero => Decimal128Value.NaN
+  | Decimal128Value.NegZero, Decimal128Value.PosZero => Decimal128Value.NaN
+  | _, Decimal128Value.PosZero => if isNegative x then Decimal128Value.NegInfinity else if isNegative x then Decimal128Value.NegInfinity else Decimal128Value.PosInfinity
+  | Decimal128Value.PosZero, Decimal128Value.NegZero => Decimal128Value.NaN
+  | Decimal128Value.NegZero, Decimal128Value.NegZero => Decimal128Value.NaN
+  | _, Decimal128Value.NegZero => if isNegative x then Decimal128Value.PosInfinity else Decimal128Value.NegInfinity
+  | Decimal128Value.PosZero, _ => if isNegative y then Decimal128Value.NegZero else Decimal128Value.PosZero
+  | Decimal128Value.NegZero, _ => if isNegative y then Decimal128Value.PosZero else Decimal128Value.NegZero
+  | Decimal128Value.Rational x, Decimal128Value.Rational y => RoundToDecimal128Domain (x.val / y.val) RoundingMode.halfEven
