761f318fc285bdbaa31f80091a2b2b6e45fd67e4
Jesse Alama <hello@jessealama.net>
2025-02-01 11:23:04 +0100

    New function for mapping positive rationals to decimals
    Also, rename placeholder "fooTheorem" to something more sensible.

--- Decimal128/Basic.lean
+++ Decimal128/Basic.lean
@@ -86,10 +86,20 @@ def Decimal128Negate (x : Decimal128Value) : Decimal128Value :=
   | Decimal128Value.NegZero => Decimal128Value.PosZero
   | Decimal128Value.Rational x => Decimal128Value.Rational (-x)
 
-theorem ratTrichotomy (x : Rat) : x < 0 ∨ x = 0 ∨ x > 0 :=
-  by sorry
+theorem ratTrichotomy (x : Rat) : x < 0 ∨ x = 0 ∨ x > 0 := by
+  have h := le_total x 0
+  cases h with
+  | inl h1 => have h2 := lt_or_eq_of_le h1
+              cases h2 with
+              | inl h3 => exact Or.inl h3
+              | inr h4 => exact Or.inr (Or.inl h4)
+  | inr h1 => have h2 := lt_or_eq_of_le h1
+              cases h2 with
+              | inl h3 => exact Or.inr (Or.inr h3)
+              | inr h4 => have h5: x = 0 := by simp [h4]
+                          exact Or.inr (Or.inl h5)
 
-theorem fooTheorem (x : Rat) : x ≠ 0 ∧ ¬(x < 0) → x > 0 :=
+theorem ne_nlt_gt (x : Rat) : x ≠ 0 ∧ ¬(x < 0) → x > 0 :=
   by
   intro h
   obtain ⟨nonZero, nonNegative⟩ := h
@@ -99,27 +109,18 @@ theorem fooTheorem (x : Rat) : x ≠ 0 ∧ ¬(x < 0) → x > 0 :=
     | inl h3 => contradiction
     | inr h4 => assumption
 
-def RoundToDecimal128Domain (v : Rat) (r : RoundingMode) : Decimal128Value :=
-  if z: v = 0
-  then Decimal128Value.PosZero
-  else if n: v < 0
-  then
-    let reverseRoundingMode := ReverseRoundingMode r
-    let d := RoundToDecimal128Domain (-v) reverseRoundingMode
-    match d with
-    | Decimal128Value.PosInfinity => Decimal128Value.NegInfinity
-    | Decimal128Value.PosZero => Decimal128Value.NegZero
-    | Decimal128Value.Rational q => Decimal128Value.Rational (-q)
-    | _ => Decimal128Value.NaN
-  else
-    have positive: v > 0 := by
-      apply fooTheorem v ⟨z, n⟩
-    let vPos : PositiveRational := ⟨v, positive⟩
-    match rationalExponent v with
+theorem abs_neg_pos (x : Rat) : x < 0 → |x| > 0 := by
+  intro h
+  have h1 := abs_pos_of_neg h
+  exact h1
+
+def RoundPositiveToDecimal128Domain (v : PositiveRational) (r : RoundingMode) : Decimal128Value :=
+    let v' : Rat := v.1
+    match rationalExponent v' with
     | some e =>
         let te : Int := max (e - (maxSignificantDigits - 1)) minDenomalizedExponent
-        let m : Rat := v * (rat10 ^ (0 - te))
-        let rounded := ApplyRoundingModeToPositive vPos r
+        let m : Rat := v' * (rat10 ^ (0 - te))
+        let rounded := ApplyRoundingModeToPositive v r
         if rounded = 0
         then Decimal128Value.PosZero
         else if rounded = 10 ^ maxSignificantDigits
@@ -130,6 +131,28 @@ def RoundToDecimal128Domain (v : Rat) (r : RoundingMode) : Decimal128Value :=
           let y : SuitableRationals := ⟨x, suitable⟩
           Decimal128Value.Rational y
     | _ => Decimal128Value.NaN
+
+def RoundToDecimal128Domain (v : Rat) (r : RoundingMode) : Decimal128Value :=
+  if z: v = 0
+  then Decimal128Value.PosZero
+  else if n: v < 0
+  then
+    let reverseRoundingMode := ReverseRoundingMode r
+    let v' := |v|
+    have positive: v' > 0 := by
+      apply abs_neg_pos v n
+    let vPos : PositiveRational := ⟨v', positive⟩
+    let d := RoundPositiveToDecimal128Domain vPos reverseRoundingMode
+    match d with
+    | Decimal128Value.PosInfinity => Decimal128Value.NegInfinity
+    | Decimal128Value.PosZero => Decimal128Value.NegZero
+    | Decimal128Value.Rational q => Decimal128Value.Rational (-q)
+    | _ => Decimal128Value.NaN
+  else
+    have positive: v > 0 := by
+      apply ne_nlt_gt v ⟨z, n⟩
+    let vPos : PositiveRational := ⟨v, positive⟩
+    RoundPositiveToDecimal128Domain vPos r
 
 def add (x : Decimal128Value) (y : Decimal128Value) : Decimal128Value :=
   match x, y with
