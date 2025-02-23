70c285eb1ca443ec449b1e06c13365ae274ef592
Jesse Alama <hello@jessealama.net>
2025-02-22 18:58:15 -0800

    Start to add correctness theorems
    Unproven, for now.

---
+++ Decimal128/Theorems.lean
@@ -0,0 +1,78 @@
+import Mathlib
+import Decimal128.Basic
+import Decimal128.Arithmetic
+
+theorem negationCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable (-p)
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable (-p)),
+  negate (Decimal128Value.Rational ⟨p, s1⟩)
+  = Decimal128Value.Rational ⟨-p, s2⟩
+:= by sorry
+
+theorem absoluteValueCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable |p|
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable |p|),
+  absoluteValue (Decimal128Value.Rational ⟨p, s1⟩)
+  = Decimal128Value.Rational ⟨|p|, s2⟩
+:= by sorry
+
+theorem additionCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable q
+∧ isRationalSuitable (p + q)
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable q) (s3 : isRationalSuitable (p + q)),
+  add (Decimal128Value.Rational ⟨p, s1⟩)
+      (Decimal128Value.Rational ⟨q, s2⟩)
+  = Decimal128Value.Rational ⟨p + q, s3⟩
+:= by sorry
+
+theorem subtractionCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable q
+∧ isRationalSuitable (p - q)
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable q) (s3 : isRationalSuitable (p - q)),
+  add (Decimal128Value.Rational ⟨p, s1⟩)
+      (Decimal128Value.Rational ⟨q, s2⟩)
+  = Decimal128Value.Rational ⟨p - q, s3⟩
+:= by sorry
+
+theorem multiplicationCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable q
+∧ isRationalSuitable (p * q)
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable q) (s3 : isRationalSuitable (p * q)),
+  add (Decimal128Value.Rational ⟨p, s1⟩)
+      (Decimal128Value.Rational ⟨q, s2⟩)
+  = Decimal128Value.Rational ⟨p * q, s3⟩
+:= by sorry
+
+theorem divisionCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable q
+∧ isRationalSuitable (p / q)
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable q) (s3 : isRationalSuitable (p / q)),
+  add (Decimal128Value.Rational ⟨p, s1⟩)
+      (Decimal128Value.Rational ⟨q, s2⟩)
+  = Decimal128Value.Rational ⟨p / q, s3⟩
+:= by sorry
+
+theorem remainderCorrect (p : Rat) (q : Rat) :
+  isRationalSuitable p
+∧ isRationalSuitable q
+∧ isRationalSuitable (p % q)
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable q) (s3 : isRationalSuitable (p % q)),
+  add (Decimal128Value.Rational ⟨p, s1⟩)
+      (Decimal128Value.Rational ⟨q, s2⟩)
+  = Decimal128Value.Rational ⟨p % q, s3⟩
+:= by sorry
+
+theorem scale10Correct (p : Rat) (n : Nat) :
+  isRationalSuitable p
+∧ isRationalSuitable (p ^ (10 * n))
+→ ∃ (s1 : isRationalSuitable p) (s2 : isRationalSuitable (p ^ (10 * n))),
+  scale10 (Decimal128Value.Rational ⟨p, s1⟩)
+          n
+  = Decimal128Value.Rational ⟨p ^ (10 * n), s2⟩
+:= by sorry
